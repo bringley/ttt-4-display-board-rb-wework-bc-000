fb79ce18b37be3a9934274a1fa43a1c96052a2d3
Brian Ringley <brian.ringley@wework.com>
2018-02-11 22:03:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board()
+def display_board(board)
   #line 1 
-  puts "   |   |   ")
+  puts "  #{board[0]} |  #{board[1]} |   ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq("   |   |   ")
       expect(rows[3]).to eq("-----------")
