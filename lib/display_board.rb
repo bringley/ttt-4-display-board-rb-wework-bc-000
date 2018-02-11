64e8fbfa20dc8ab6f311d8255a5056aaf4392544
Brian Ringley <brian.ringley@wework.com>
2018-02-11 22:05:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+# assumption is that board has 9 spaces therefore takes an array argument of length 9
 def display_board(board)
-  #line 1 
-  puts "  #{board[0]} |  #{board[1]} |   ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
+  break = "-----------"
+  puts "  #{board[0]} |  #{board[1]} | #{board[2]}  "
+  puts break
+  puts "  #{board[3]} |  #{board[4]} | #{board[5]}  "
+  puts break
+  puts "  #{board[6]} |  #{board[7]} | #{board[8]}  "
 end
